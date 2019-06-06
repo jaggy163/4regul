faff151c04102b06d99db2e773dd9fe69de9de78
anton.mantrov <mantrov2009@mail.ru>
2019-06-06 16:58:02 +0400

    Initial commit

--- src/main/java/Fifteen/GUI.java
+++ src/main/java/Fifteen/GUI.java
@@ -7,14 +7,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     JPanel panel = new JPanel();
     public int turnOut;
+
 
     GUI() {
         super("Victory");
         JPanel panel = new JPanel();
-        setSize(400,530);
-        panel.add(new JLabel("Победа!"));
+        int width = 150;
+        int height = 120;
+        int locationX = (screenSize.width-width)/2;
+        int locationY = (screenSize.height-height)/2;
+
+        setSize(width,height);
+        setLocation(locationX, locationY);
+
+        JButton exitButton = new JButton("Выход");
+        exitButton.addActionListener(e -> System.exit(0));
+        JLabel label = new JLabel("Победа!");
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        panel.setLayout(new GridLayout(2,0));
+        panel.add(label);
+        panel.add(exitButton);
         add(panel);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -23,7 +40,15 @@ public class GUI extends JFrame {
         super("Fifteen");
         JPanel secondPanel = new JPanel();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(400,530);
+
+        int width = 400;
+        int height = 530;
+        int locationX = (screenSize.width-width)/2;
+        int locationY = (screenSize.height-height)/2;
+
+        setSize(width,height);
+        setLocation(locationX, locationY);
+
         panel.setPreferredSize(new Dimension(400,400));
         secondPanel.setPreferredSize(new Dimension(400,100));
         secondPanel.setLayout(new GridLayout(3,3));
@@ -35,7 +60,7 @@ public class GUI extends JFrame {
         try {
             textField.addActionListener(e -> doTurn(Integer.valueOf(textField.getText())));
         } catch (Exception e) {
-            textField.removeAll();
+            textField.setText("");
             e.printStackTrace();
         }
 
@@ -43,18 +68,21 @@ public class GUI extends JFrame {
         try {
             buttonDoTurn.addActionListener(e -> doTurn(Integer.valueOf(textField.getText())));
         } catch (Exception e) {
-            textField.removeAll();
+            textField.setText("");
             e.printStackTrace();
         }
+        JLabel label = new JLabel("Ваш ход:");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
 
         secondPanel.add(new JLabel(" "));
-        secondPanel.add(new JLabel("Ваш ход:"), BorderLayout.CENTER);
+        secondPanel.add(label);
         secondPanel.add(new JLabel(" "));
         secondPanel.add(new JLabel(" "));
         secondPanel.add(textField);
         secondPanel.add(new JLabel(" "));
         secondPanel.add(new JLabel(" "));
-        secondPanel.add(buttonDoTurn, BorderLayout.CENTER);
+        secondPanel.add(buttonDoTurn);
         secondPanel.add(new JLabel(" "));
 
         panel.setLayout(layout);
@@ -86,4 +114,5 @@ public class GUI extends JFrame {
     public void doTurn(int turn) {
         Main.turnFromGUI = turn;
     }
+
 }
